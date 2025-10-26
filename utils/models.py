ed8f7138bbd37eaf92617c9ea1ce031b52d2f076
Alex1Iv <ivanov.politehnik@gmail.com>
2025-10-26 22:55:07 +0600

    upd

---
+++ utils/models.py
@@ -0,0 +1,86 @@
+import os
+
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, BackupAndRestore
+from tensorflow.keras import layers, models
+
+def callbacks(model_name:str, path_models:str,
+    reduce_patience:int,
+    stop_patience:int,
+    monitor:str,
+    verbose:bool,
+    mode:str,
+    save_best_only:bool,
+    save_weights_only:bool,
+    restore_best_weights:bool,
+    cooldown_epochs:int,
+    lr:float,
+    factor:float
+    ):
+    """ Manages the learning process of our model
+
+    Args:
+        model_name (str): model name
+        path_models (str): path to save
+        reduce_patience (int): decreases the lr when metrics doesn't change
+        stop_patience (int): the number of epochs before the learning process is terminated if the metric doesn't change
+        monitor (str): metric to monitor
+        verbose (bool): shows the output
+        mode (str): study mode
+        save_best_only (bool): saves models with improved quality
+        save_weights_only (bool): _description_
+        restore_best_weights (bool): _description_
+        cooldown_epochs (int): wait period when 
+        lr (float): the learning rate
+        factor (float): learning rate decrease factor (0,1)
+        min_lr(float): minimal learning rate (Default: 1e-8)
+    """
+
+    # End training if the metric doesn't imporve
+    earlystop = EarlyStopping(
+        monitor=monitor,
+        mode=mode,
+        patience=stop_patience,
+        restore_best_weights=restore_best_weights,
+        verbose=verbose
+    )
+
+    # Decrease learning rate if the metric doesn't improve 
+    reduce_lr = ReduceLROnPlateau(
+        monitor=monitor,        # metric to be monitored
+        mode=mode,              
+        min_lr=lr/1000,
+        factor=factor, 
+        patience=reduce_patience,
+        cooldown=cooldown_epochs,
+        verbose=verbose
+    )
+    
+    checkpoint = ModelCheckpoint(
+        filepath=os.path.join(path_models, model_name + '.weights.h5'), #full: '_model.keras' subclass:.h5
+        #save_format="tf", # saves a subclassed model
+        save_best_only=save_best_only, 
+        save_weights_only=save_weights_only,
+        monitor=monitor, 
+        mode=mode,
+        verbose=verbose        
+    )
+    
+    backup = BackupAndRestore(
+        # The path where your backups will be saved. Make sure the
+        # directory exists prior to invoking `fit`.
+        os.path.join(path_models),
+        # How often you wish to save a checkpoint. Providing "epoch"
+        # saves every epoch, providing integer n will save every n steps.
+        save_freq="epoch",
+        # Deletes the last checkpoint when saving a new one.
+        delete_checkpoint=True,
+    )
+    # # reduces learnign rate smoothly
+    # scheduler = LearningRateScheduler(
+    #     schedule=smooth_decay(epoch, lr), 
+    #     verbose=config.callbacks.verbose
+    # )
+
+    return [checkpoint, earlystop, reduce_lr, backup] 
